70dae0c4416e2f9795172a682ddf16342e430411
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-05-21 00:46:25 +0900

    first commit

---
+++ lib/fluent/plugin/raven/decoder.rb
@@ -0,0 +1,11 @@
+require "fluent/plugin/raven/decoder/version"
+
+module Fluent
+  module Plugin
+    module Raven
+      module Decoder
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/fluent/plugin/raven/decoder/version.rb
@@ -0,0 +1,9 @@
+module Fluent
+  module Plugin
+    module Raven
+      module Decoder
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
